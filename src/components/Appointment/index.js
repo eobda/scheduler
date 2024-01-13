a554fd0e870e0c7437fc41db88974883a6001ccb
maddie <lee.maddie@gmail.com>
2024-01-13 17:45:51 -0500

    Add mode constants

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -15,6 +15,12 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const EDIT = "EDIT";
+  const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
