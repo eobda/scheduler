aae8436cb5bcf9a261ac9251daa3e7eebd9c7015
maddie <lee.maddie@gmail.com>
2024-01-18 08:56:41 -0500

    Import and describe Appointment component in test

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -2,8 +2,12 @@ import React from "react";
 
 import { render } from "@testing-library/react";
 
-import Application from "components/Application";
+import Appointment from "components/Appointment";
 
-test("renders without crashing", () => {
-  render(<Application />);
+describe("Appointment", () => {
+  
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
 });
