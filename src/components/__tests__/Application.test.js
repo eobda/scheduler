754eb7a671ee991cef8ec2797d2ebe0db2515ed9
maddie <lee.maddie@gmail.com>
2024-01-23 11:19:40 -0500

    Clean up spacing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -17,24 +17,23 @@ describe("Application", () => {
     });
   });
 
-  /*
-  Click the "Add" button on the first empty appointment.
-  Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
-  Click the first interviewer in the list.
-  Click the "Save" button on that same appointment.
-  */
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container } = render(<Application />);
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    const appointment = getAllByTestId(container, "appointment")[0];
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+
     fireEvent.click(getByAltText(appointment, "Add"));
+
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
     fireEvent.click(getByText(appointment, "Save"));
+
     console.log(prettyDOM(appointment));
   });
 
