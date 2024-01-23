c456084311aeed2dcf4d98752f13f0e082205bbb
maddie <lee.maddie@gmail.com>
2024-01-23 08:58:45 -0500

    Add describe block

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,15 +6,19 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("renders without crashing", () => {
-  render(<Application />);
-});
+describe("Application", () => {
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
-
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  xit("renders without crashing", () => {
+    render(<Application />);
   });
+  
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
+  });
+
 });
