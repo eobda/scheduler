b699a13eed5fbc6174a432c2a288a70cc823bcd2
maddie <lee.maddie@gmail.com>
2024-01-24 14:03:51 -0500

    Remove debug and comments

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -70,51 +70,36 @@ describe("Application", () => {
   });
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
-    // 1. Render the application
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
 
-    // 2. Wait until text "Archie Cohen" is in the document
     await waitForElement(() => container, "Archie Cohen");
 
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
 
-    // 3. Click the "Edit" button on an interview
     fireEvent.click(queryByAltText(appointment, "Edit"));
 
-    // 4. Change the student name to "Eijun Sawamura"
     fireEvent.change(getByPlaceholderText(appointment, "Enter Student Name"), {
       target: { value: "Eijun Sawamura" }
     });
-
-    // 5. Change the interviewer to "Sylvia Palmer"
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     
-    // 6. Click "Save"
     fireEvent.click(getByText(appointment, "Save"));
-
-    // 7. Check that "Saving" text is shown in document
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
-    // 8. Wait until "Saving" text is removed
     await waitForElementToBeRemoved(() => getByText(appointment, "Saving"));
 
-    // 9. Check that "Eijun Sawamura" is in the document
     expect(queryByText(appointment, "Eijun Sawamura")).toBeInTheDocument();
     expect(queryByText(appointment, "Archie Cohen")).not.toBeInTheDocument();
 
-    // 10. Check that the interviewer has changed to "Sylvia Palmer"
     expect(queryByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
     expect(queryByText(appointment, "Tori Malcolm")).not.toBeInTheDocument();
 
-    // 11. Check that DayListItem for "Monday" has the text "1 spot remaining"
     const day = getAllByTestId(container, "day").find(
       day => queryByText(day, "Monday")
     );
     expect(queryByText(day, "1 spot remaining")).toBeInTheDocument();
-
-    debug();
   });
 
 });
