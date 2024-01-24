49e784a13fac90c3b57bc95220cabc1c6e904d96
maddie <lee.maddie@gmail.com>
2024-01-24 12:36:25 -0500

    Write test plan for editing interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -62,4 +62,18 @@ describe("Application", () => {
     expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
   });
 
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {
+    // 1. Render the application
+    // 2. Wait until text "Archie Cohen" is in the document
+    // 3. Click the "Edit" button on an interview
+    // 4. Change the student name to "Eijun Sawamura"
+    // 5. Change the interviewer to "Sylvia Palmer"
+    // 6. Click "Save"
+    // 7. Check that "Saving" text is shown in document
+    // 8. Wait until "Saving" text is removed
+    // 9. Check that "Eijun Sawamura" is in the document
+    // 10. Check that the interviewer has changed to "Sylvia Palmer"
+    // 11. Check that DayListItem for "Monday" has the text "1 spot remaining"
+  });
+
 });
