aa745cf62d88086c49b6014303ff7f25e94718a6
maddie <lee.maddie@gmail.com>
2024-01-27 18:57:18 -0500

    Click save button

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -11,7 +11,8 @@ describe("Appointments", () => {
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
 
-    cy.get("[alt='Sylvia Palmer']")
-      .click();
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
   });
 });
