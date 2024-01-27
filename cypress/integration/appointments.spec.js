b6882772ecbd1cb88a2a211e4b17c0cb99fda37c
maddie <lee.maddie@gmail.com>
2024-01-27 18:39:20 -0500

    Add name typing into input field

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -7,5 +7,8 @@ describe("Appointments", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
+
+    cy.get("[data-testid=student-name-input]")
+      .type("Lydia Miller-Jones");
   });
 });
