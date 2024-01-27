219b290c0ffb975e8cb9d228e75892038016ff24
maddie <lee.maddie@gmail.com>
2024-01-27 18:44:22 -0500

    Click on interviewer

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -10,5 +10,8 @@ describe("Appointments", () => {
 
     cy.get("[data-testid=student-name-input]")
       .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Sylvia Palmer']")
+      .click();
   });
 });
